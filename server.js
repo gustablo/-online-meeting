c50449f7eb619c38e8b3e0fbe7259494ee670e4a
gustablo <gpinheiro2001@gmail.com>
2021-05-30 02:49:57 -0300

    config deploy on heroku

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/mandala-open-platform'));
+app.use(express.static(__dirname + '/dist/online-meeting'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/mandala-open-platform/index.html'));
+res.sendFile(path.join(__dirname+'/dist/online-meeting/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
