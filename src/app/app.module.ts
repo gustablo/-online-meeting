ede9005581c3c3d60879906ebd705c07994609f3
gustablo <gpinheiro2001@gmail.com>
2021-05-30 02:39:10 -0300

    config deply

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,9 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import { environment } from 'src/environments/environment';
 
-const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
+const config: SocketIoConfig = { url: environment.socket, options: {} };
 
 @NgModule({
   declarations: [
